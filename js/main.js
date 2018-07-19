ed67dfcef8173d80e53b26e83c1c033ead0e42f9
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-20 00:57:30 +0300

    Add function to hide tooltip when caret button is clicked in tweet component

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(function() {
-    $('[data-toggle="tooltip"]').tooltip({
+    //Apply tooltip for each element with data-toggle attr. = tooltip
+    $('[data-toggle="tooltip"], #tweetMoreBtn').tooltip({
         delay: {
             show: 200,
             hide: 0
         },
         animation: false
+    })
+
+    // Hide tooltip when drop-down menu is shown
+    $('#tweetMoreBtn').on('click', function() {
+        $('#tweetMoreBtn').tooltip('hide')
     })
 })
